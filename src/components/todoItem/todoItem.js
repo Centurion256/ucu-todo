2a2e67d033e09cdd1cc8a4fae2cd14d7d5185894
Centurion256 <Vkuchynskyy@gmail.com>
2020-04-01 01:58:12 +0300

    added StepanError, no completed TODO duplication

--- src/components/todoItem/todoItem.js
+++ src/components/todoItem/todoItem.js
@@ -12,16 +12,34 @@ import Stepan from '/src/lib/stepan.js';
 
 export default class TodoItem extends Stepan.Component {
   render({isDone, title}) { // render will always accept data to render
-    const rootElement = Stepan.createElement('li', this.parent, { class: isDone && 'completed' });
-    const todoViewContainer = Stepan.createElement('div', rootElement, { class: 'view' });
+
 
     // TODO: Input must be checked if todo item is done
-    Stepan.createElement('input', todoViewContainer, {class: "toggle", type: "checkbox"});
 
-    Stepan.createElement('label', todoViewContainer, {innerText: title});
-    Stepan.createElement('button', todoViewContainer, {class: "destroy"});
-    Stepan.createElement('input', todoViewContainer, { class: "edit", value: title });
+    //try-catch is used to properly handle incorrect/unimportant input.
+    try {
+      //Get an iterator over all completed todos:
+      let todos = this.parent.querySelectorAll("li.completed").values(); 
+      for (let todo of todos) {
+        let label = todo.querySelector("div > label");
+        if (label.textContent == title)
+        {
+          throw new Error(`\"${title}\" TODO element has already been completed.`);
+        }
+      }
+      const rootElement = Stepan.createElement('li', this.parent, { class: isDone && 'completed' });
+      const todoViewContainer = Stepan.createElement('div', rootElement, { class: 'view' });
+      Stepan.createElement('input', todoViewContainer, {class: "toggle", type: "checkbox"});
+  
+      Stepan.createElement('label', todoViewContainer, {innerText: title});
+      Stepan.createElement('button', todoViewContainer, {class: "destroy"});
+      Stepan.createElement('input', todoViewContainer, { class: "edit", value: title });
+  
+      return rootElement
 
-    return rootElement
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   }
 }
--- src/lib/stepan.js
+++ src/lib/stepan.js
@@ -3,7 +3,10 @@ export default class Stepan {
   static createElement(element, parent, attributes = {}) {
     // TODO: check if this is a valid tag name
     const newElement = document.createElement(element);
-
+    if (!(newElement instanceof HTMLElement))
+    {
+      throw new StepanError("Invalid newElement.");
+    }
     const { innerHTML, innerText } = attributes;
 
     for (let attribute in attributes) {
@@ -24,12 +27,23 @@ export default class Stepan {
 
   static Component = class {
     constructor(parent) {
+      if (!(parent instanceof HTMLElement) || parent == null)
+      {
+        throw new StepanError("No or invalid parent element."); //should remain unchecked
+      }
+      this.parent = parent;
 
       // TODO: 1. Create StepanError class to define all framework errors
       //       2. throw an error if parent is null or undefined, or if it's not a valid DOM object
 
-      this.parent = parent;
     }
+  
+  static StepanError = class extends Error {
+    constructor(errorMessage) {
+      super(errorMessage); 
+      this.name = "StepanError";
+    }
+  }
 
     // TODO (Bonus): Ensure that every component returns a top-level root element
   }
